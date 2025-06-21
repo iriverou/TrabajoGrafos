99411cd45dc94598aad45f464d2156a215b3601f
Ihan Kael Rivero Araos <irivero@utem.cl>
2025-06-21 14:20:24 -0400

    configuracion para enlazar con github

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://iriverou.github.io',
+    base: '/TrabajoGrafos',
+});
